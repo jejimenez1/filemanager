8a4cee89e41b547d7462bc77ca41571d3415e066
Jesus Jimenez <jesujimenezochoa@gmail.com>
2024-12-17 22:58:32 +0000

    finished2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   res.json({
-    name: req.file.originalname,
+    name: req.file.filename,
     type: req.file.mimetype,
     size: req.file.size
   })
